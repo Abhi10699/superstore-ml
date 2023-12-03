1017b2568476c9ba221eb84d925636a8897de42d
codise-abhi <patelabhi10699@gmail.com>
2023-12-03 14:06:10 -0500

    feat: initialised font loaders and prisma orm

---
+++ app/utils/font-loaders.tsx
@@ -0,0 +1,6 @@
+import { Fahkwang, DM_Sans } from "next/font/google";
+
+const h1 = Fahkwang({ weight: "700", subsets: ["latin"] });
+const paragraph = DM_Sans({ weight: "400", subsets: ["latin"] });
+
+export { h1, paragraph };
